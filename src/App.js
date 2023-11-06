be9f108be524ccd6d2084d1483e1154494729e9a
Subodh Kute <subodhkute07@gmail.com>
2023-11-06 21:07:56 +0530

    fetching news category wise

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import { News } from "./components/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-    <div>
-      <Navbar/>
-      <News pagesize={6} category={'sports'}/>
-    </div>
-    )
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          {/* <News pagesize={6} category="sports" /> */}
+          <Routes>
+          <Route exact path="/" element={<News key="general" pagesize={6} category="general" />}/>
+            <Route exact path="/business" element={<News key="business" pagesize={6} category="business" />}/>
+            <Route exact path="/entertainment" element={<News key="entertainment" pagesize={6} category="entertainment" />}/>
+            <Route exact path="/general" element={<News key="general" pagesize={6} category="general" />}/>
+            <Route exact path="/health" element={<News key="health" pagesize={6} category="health" />}/>
+            <Route exact path="/science" element={<News key="science" pagesize={6} category="science" />}/>
+            <Route exact path="/sports" element={<News key="sports" pagesize={6} category="sports" />}/>
+            <Route exact path="/technology" element={<News key="technology" pagesize={6} category="technology" />}/>
+            {/* <Route path="*" element={<NoPage />}></Route> */}
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
