6679cd4d222d00eb4f39fdc7ec334290d41ea626
Subodh Kute <subodhkute07@gmail.com>
2023-11-08 14:55:47 +0530

    using axios instead of fetch

--- src/components/News.js
+++ src/components/News.js
@@ -1,77 +1,71 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-// export class News extends Component {
+
   const News = (props) => {
 
     const [articles, setArticles] = useState([])
     const [loading, setLoading] = useState(true)
     const [page, setPage] = useState(1)
     const [totalResults, setTotalResults] = useState(0)
-    // document.title = `NewsWave - ${this.capitalizeFirstLetter(props.category)}`;
 
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     articles: [],
-  //     loading: true,
-  //     page: 1,
-  //     totalResults: 0,
-  //   };
-  //   document.title = `NewsWave - ${this.capitalizeFirstLetter(
-  //     props.category
-  //   )}`;
-  // }
 
   const updateNews = async () => {
     props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pagesize}`;
     // this.setState({ loading: true });
     setLoading(true)
-    let data = await fetch(url);
-    props.setProgress(30);
-    let parsedData = await data.json();
-    props.setProgress(70);
-    setArticles(parsedData.articles);
-    setTotalResults(parsedData.totalResults);
-    setLoading(false);
-    props.setProgress(100);
+    // let data = await fetch(url);
+    // props.setProgress(30);
+    // let parsedData = await data.json();
+    try{
+      let res = await axios.get(url);
+      props.setProgress(30);
+      let parsedData = res.data;
+      props.setProgress(70);
+      setArticles(parsedData.articles);
+      setArticles(parsedData.articles);
+      setTotalResults(parsedData.totalResults);
+      setLoading(false);
+      props.setProgress(100);
+    }catch (error) {
+      // Handle errors here
+      console.error("API request failed:", error);
+      setLoading(false);
+    }
   }
 
   useEffect(() => {
     updateNews();
   }, [])
 
-  // const componentDidMount = async() => {
-  //   //we removed duplicate code
-  //   this.updateNews();
-  // }
-
   const fetchMoreData = async () => {
     // this.setState({page: this.state.page + 1})
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pagesize}`;
     setPage(page + 1)
-    let data = await fetch(url);
-    let parsedData = await data.json();
+    // let data = await fetch(url);
+    // let parsedData = await data.json();
+  
+    let res = await axios.get(url)
+                         .catch((error) => {console.error("API request failed:", error);});
+    let parsedData = res.data;
     setArticles(articles.concat(parsedData.articles))
     setTotalResults(parsedData.totalResults)
-    // this.setState({
-    //   articles: this.state.articles.concat(parsedData.articles),
-    //   totalResults: parsedData.totalResults
-    // });
+  
   };
 
+  document.title = `NewsWave - ${capitalizeFirstLetter(props.category)}`;
   
   return (
     <>
-      <h2 className="text-center" style={{ margin: "35px 0px" }}>
+      <h2 className="text-center" style={{ margin: "35px 0px", marginTop: '90px' }}>
         NewsWave - Top {capitalizeFirstLetter(props.category)}{" "}
         Headlines
       </h2>
